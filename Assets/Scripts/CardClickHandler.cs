4465a814d5288d53e043f7a8773e1989693595d7
Phoenixxi <rowan809xi@gmail.com>
2025-01-25 15:59:30 -0700

    Card Highlight and scale added

--- Assets/Scripts/CardClickHandler.cs
+++ Assets/Scripts/CardClickHandler.cs
@@ -1,38 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using CardNamespace;
+using UnityEngine.UI;
 
-public class CardClickHandler : MonoBehaviour, IPointerClickHandler
+public class CardClickHandler : MonoBehaviour, IPointerClickHandler, IPointerExitHandler, IPointerEnterHandler
 {
+    private Canvas canvas;
     private bool isSelected = false;
+     public float moveUpOffset = 30f;
+     private float selectScale = 1.1f;
+
+    // Original positions
+     private Vector3 originalScale;
     private Vector3 originalPosition;
     private Vector3 selectedPosition;
 
-    // Offset to move the card up when selected
-    public float moveUpOffset = 30f;
+    [SerializeField] private GameObject highlightEffect;
 
-    private void Start()
-    {
-        // Save the original position of the card
-        originalPosition = transform.localPosition;
-        // Calculate the selected position
-        selectedPosition = originalPosition + new Vector3(0, moveUpOffset, 0);
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        // Toggle the selection state
-        isSelected = !isSelected;
-
-        // Move the card based on the selection state
-        if (isSelected)
+        private void Start()
         {
-            transform.localPosition = selectedPosition;
+            // Save the original position of the card
+            originalPosition = transform.localPosition;
+            originalScale = transform.localScale;
+            // Calculate the selected position
+            selectedPosition = originalPosition + new Vector3(0, moveUpOffset, 0);
+            //highlightEffect.SetActive(true);
+            
         }
-        else
+
+        void Awake()
         {
-            transform.localPosition = originalPosition;
+            canvas = GetComponent<Canvas>();
         }
-    }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // Toggle the selection state
+            isSelected = !isSelected;
+
+            // Move the card based on the selection state
+            if (isSelected)
+            {
+                transform.localPosition = selectedPosition;
+            }
+            else
+            {
+                transform.localPosition = originalPosition;
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerEnter called: Mouse entered the card.");
+            highlightEffect.SetActive(true);
+            transform.localScale = originalScale * selectScale; 
+        }      
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerExit called");
+            // Deactivate the CardHighlight image
+            highlightEffect.SetActive(false);
+
+            // Reset the scale of the card
+            transform.localScale = originalScale;
+        }      
 }
--- Assets/Scripts/CardMovement.cs
+++ Assets/Scripts/CardMovement.cs
@@ -1,18 +1,100 @@
+/*
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardMovement : MonoBehaviour
+public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private RectTransform rectTransform;
+   private Canvas canvas;
+   
+   // Mouse Pointer Position
+   private Vector2 originalLocalPointerPosition;
+   private Vector3 originalPanelLocalPosition;
+   private Vector3 originalScale;
+   private int currentState = 0;
+   private Quaternion originalRotation;
+   private Vector3 originalPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [SerializeField] private float selectScale = 1.1f;
+   [SerializeField] private Vector2 cardPlay;
+   [SerializeField] private Vector3 playPosition;
+   [SerializeField] private GameObject highlightEffect;
+   [SerializeField] private GameObject playArrow;
+
+   void Awake()
+   {
+      rectTransform = GetComponent<rectTransform>();
+      canvas = GetComponent<Canvas>();
+      originalScale = rectTransform.localScale;
+      originalPosition = rectTransform.localPosition;
+      originalRotation = rectTransform.localRotation;
+   }
+
+   void Update()
+   {
+      switch(currentState)
+      {
+         case 1:
+            HandleHoverState();
+            break;
+         case 2:
+            HandleDragState();
+            if(!Input.GetMouseButton(0))  // Check if mouse button is released
+               TransitionToState0();
+            break;
+         case 3:
+            HandlePlayState();
+            if(!Input.GetMouseButton(0))  // Check if mouse button is released
+               TransitionToState0();
+            break;
+      }
+   }
+
+   private void TransitionToState0()
+   {
+      currentState = 0;
+      
+   }
+
+   private void OnPointerEnter(PointerEventData eventData)
+   {
+      if(currentState == 0)
+      {
+         originalPosition = rectTransform.localPosition;
+         originalRotation = rectTransform.localRotation;
+         originalScale = rectTransform.localScale;
+
+         currentState = 1;
+      }
+   }
+
+   public void OnPointerExit(PointerEventData eventData)
+   {
+      if(currentState == 1)
+      {
+         currentState = 0;
+         TransitionToState0();
+      }
+   }
+
+   public void OnPointerDown(PointerEventData eventData)
+   {
+      if(currentState == 1)
+      {
+         currentState = 2;
+         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
+         originalPanelLocalPosition = rectTransform.localPosition;
+      }
+   }
+
+   private void HandleHoverState()
+   {
+      highlightEffect.SetActive(true);
+      rectTransform.localScale = originalScale * selectScale; 
+   }
 }
+
+*/
