e9350495d17f534348a19a16347b7e8ea5009ffe
Phoenixxi <rowan809xi@gmail.com>
2025-01-26 18:12:49 -0700

    Set up more managers
    Options, Audio, and Deck

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CardNamespace;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
     private int teamHealth;
+    
+    public OptionsManager OptionsManager {get; private set;}
+    public AudioManager AudioManager {get; private set;}
+    public DeckManager DeckManager {get; private set;}
 
     private void Awake()
     {
@@ -14,11 +19,62 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            InitializeManagers();
         }
         else if(Instance != null)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void InitializeManagers()
+    {
+        OptionsManager = GetComponentInChildren<OptionsManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
+        DeckManager = GetComponentInChildren<DeckManager>();
+
+        if(OptionsManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/OptionsManager");
+            if(prefab == null)
+            {
+                Debug.Log($"OptionsManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                OptionsManager = GetComponentInChildren<OptionsManager>();
+            }
+        }
+
+         if(AudioManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/AudioManager");
+            if(prefab == null)
+            {
+                Debug.Log($"AudioManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                AudioManager = GetComponentInChildren<AudioManager>();
+            }
+        }
+
+         if(DeckManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/DeckManager");
+            if(prefab == null)
+            {
+                Debug.Log($"DeckManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                DeckManager = GetComponentInChildren<DeckManager>();
+            }
+        }
+            
     }
 
     public int TeamHealth
