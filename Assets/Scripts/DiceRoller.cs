9a220845a3f1b5e91539722f82320b7323a84cff
Phoenixxi <rowan809xi@gmail.com>
2025-02-07 18:49:53 -0700

    Prototype dice roller done

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoller : MonoBehaviour
+{
+    // temporary text display for the prototype
+   public Text resultText; 
+   // energy text field
+    public Text energyText; 
+
+    private void Start()
+    {
+        resultText.text = "";
+        energyText.text = "Energy: 0";
+    }
+
+    public void RollDice()
+    {
+        // Random number between 1-10
+        int diceResult = Random.Range(1, 11); 
+        StartCoroutine(ShowResult(diceResult));
+    }
+
+    private IEnumerator ShowResult(int diceResult)
+    {
+        // temporary for prototype before we get roll animation
+        resultText.text = "Rolling...";
+        yield return new WaitForSeconds(1.5f);
+        resultText.text = diceResult.ToString();
+        energyText.text = "Energy: " + diceResult;
+
+        // Clear temporary result and update Energy text
+        yield return new WaitForSeconds(1f);
+        resultText.text = "";
+    }
+}
