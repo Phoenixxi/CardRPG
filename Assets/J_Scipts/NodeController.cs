782d0a6dc94df3b49918425b0c06fea58f3a1310
Jhildt97 <jhildt97@gmail.com>
2025-02-12 18:31:39 -0700

    Can now travel scenes by clicking enter
    In the future, you'll know which scene to go to by the node ID or something, but for now, if you click enter, it'll take you to Ryan Scene, which should be DeckBuilding.

--- Assets/J_Scipts/NodeController.cs
+++ Assets/J_Scipts/NodeController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NodeController : MonoBehaviour
 {
-     public float nodeZoomLevel = 60f; // Default zoom level for this node
+    public float nodeZoomLevel = 60f; // Default zoom level for this node
     public Vector3 cameraOffset; // Custom position offset per node
     public Vector3 cameraRotation; // Custom rotation per node
+    public string sceneToLoad; // Scene name to load when entering the node
+    private bool playerIsOverNode = true; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,42 @@ public class NodeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerIsOverNode && Input.GetKeyDown(KeyCode.Return)) 
+        {
+            LoadNextScene();
+        }
+
     }
+
+    private void LoadNextScene()
+    {
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("No scene assigned for this node.");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Ensure your player has the "Player" tag
+        {
+            playerIsOverNode = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsOverNode = false;
+        }
+    }
+
 
     private void OnMouseDown()
     {
