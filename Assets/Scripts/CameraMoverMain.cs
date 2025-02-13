881c54ea99ea78755e4a850447bc8b61999153ec
Phoenixxi <rowan809xi@gmail.com>
2025-02-12 20:57:51 -0700

    Updated camera in main menu

--- Assets/Scripts/CameraMoverMain.cs
+++ Assets/Scripts/CameraMoverMain.cs
@@ -7,7 +7,7 @@ public class CameraMoverMain : MonoBehaviour
     // Assigned in inspector
    public Transform targetTransform;
     public float moveSpeed = 3f;
-     public float rotateSpeed = 5f;
+     public float rotateSpeed = 10f;
 
     private bool shouldMove = false;
 
@@ -18,7 +18,7 @@ public class CameraMoverMain : MonoBehaviour
             // Move towards the target position
             transform.position = Vector3.MoveTowards(transform.position, targetTransform.position, moveSpeed * Time.deltaTime);
             Quaternion targetRotation = targetTransform.rotation;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,  rotateSpeed * Time.deltaTime * 2 );
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,  rotateSpeed * Time.deltaTime * 5 );
 
             // Stop when both position and rotation are close enough
             if (Vector3.Distance(transform.position, targetTransform.position) < 0.01f &&
