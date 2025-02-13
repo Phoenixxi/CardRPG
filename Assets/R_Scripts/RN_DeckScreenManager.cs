0e6aedb9e7cbfc757ece9a69feec3a4751bb8629
Phoenixxi <rowan809xi@gmail.com>
2025-02-13 11:19:57 -0700

    fix compiler bugs for export

--- Assets/R_Scripts/RN_DeckScreenManager.cs
+++ Assets/R_Scripts/RN_DeckScreenManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Unity.VisualStudio.Editor;
+//using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
