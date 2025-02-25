30e5d3fae2dde00f7e21494af4f34e790fc1880c
Phoenixxi <rowan809xi@gmail.com>
2025-02-24 21:00:22 -0700

    Simple temporary fight cut scene

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using CardNamespace;
 
 public class SceneController : MonoBehaviour
 {
+     private float changeTime = 4.5f;
    public void PlayGame ()
    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -27,6 +29,14 @@ public class SceneController : MonoBehaviour
      public void ReturnMenu()
      {
           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+     }
+
+
+     private void Update()
+     {
+          changeTime -= Time.deltaTime;
+          if(changeTime <= 0)
+               SceneManager.LoadScene("Combat");
      }
 
 
