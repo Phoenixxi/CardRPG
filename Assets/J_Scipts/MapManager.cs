3e411ad1545ca07731939a9b7c55e6c63ae012c5
Jhildt97 <jhildt97@gmail.com>
2025-03-23 15:02:38 -0600

    Nodes Save, and Player Location Saves
    Bugs:
    Camera is stuck at beginning, when you go to MainMenu, the Jared scene is running in the background, at the same time as Main Menu

--- Assets/J_Scipts/MapManager.cs
+++ Assets/J_Scipts/MapManager.cs
@@ -6,12 +6,15 @@ public class MapManager : MonoBehaviour
 {public static MapManager Instance { get; private set; }
     
     public List<NodeController> nodes = new List<NodeController>();
+    public PlayerController player;
+    public CameraController cameraController;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject); // Prevents MapManager from being destroyed when changing scenes
         }
         else
         {
@@ -21,9 +24,108 @@ public class MapManager : MonoBehaviour
         PopulateNodes();
     }
 
+    private void Start()
+    {
+        //SaveGameData(); // Save 
+        // PlayerPrefs.DeleteAll();  // Reset for Testing
+
+        player = FindObjectOfType<PlayerController>();
+        cameraController = FindObjectOfType<CameraController>();
+        LoadGameData();
+    }
+
+
     private void PopulateNodes()
     {
         nodes.Clear(); // Clears existing list before adding
         nodes.AddRange(FindObjectsOfType<NodeController>());
+        nodes.Sort((node1, node2) => string.Compare(node1.name, node2.name));
     }
+
+    public void SaveGameData()
+    {
+        // Save Player Position
+        PlayerPrefs.SetFloat("Player_Pos_X", player.transform.position.x);
+        PlayerPrefs.SetFloat("Player_Pos_Y", player.transform.position.y);
+        PlayerPrefs.SetFloat("Player_Pos_Z", player.transform.position.z);
+
+        // Save Camera Position & Rotation
+        PlayerPrefs.SetFloat("Camera_Pos_X", cameraController.transform.position.x);
+        PlayerPrefs.SetFloat("Camera_Pos_Y", cameraController.transform.position.y);
+        PlayerPrefs.SetFloat("Camera_Pos_Z", cameraController.transform.position.z);
+
+        PlayerPrefs.SetFloat("Camera_Rot_X", cameraController.transform.rotation.eulerAngles.x);
+        PlayerPrefs.SetFloat("Camera_Rot_Y", cameraController.transform.rotation.eulerAngles.y);
+        PlayerPrefs.SetFloat("Camera_Rot_Z", cameraController.transform.rotation.eulerAngles.z);
+
+        // Save Camera FOV
+        PlayerPrefs.SetFloat("Camera_FOV", cameraController.mainCamera.fieldOfView);
+
+        // Save Node Data
+        foreach (var node in nodes)
+        {
+            PlayerPrefs.SetInt($"Node_{node.name}_Unlocked", node.nodeUnlocked ? 1 : 0);
+            PlayerPrefs.SetInt($"Node_{node.name}_Closed", node.isClosed ? 1 : 0);
+            PlayerPrefs.SetInt($"Node_{node.name}_ForkUnlocked", node.forkUnlocked ? 1 : 0);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadGameData()
+    {
+        // Load Player Position
+        Vector3 playerPosition = new Vector3(
+            PlayerPrefs.GetFloat("Player_Pos_X", player.transform.position.x),
+            PlayerPrefs.GetFloat("Player_Pos_Y", player.transform.position.y),
+            PlayerPrefs.GetFloat("Player_Pos_Z", player.transform.position.z)
+        );
+        player.transform.position = playerPosition;
+
+        // Load Camera Position & Rotation
+        Vector3 cameraPosition = new Vector3(
+            PlayerPrefs.GetFloat("Camera_Pos_X", cameraController.transform.position.x),
+            PlayerPrefs.GetFloat("Camera_Pos_Y", cameraController.transform.position.y),
+            PlayerPrefs.GetFloat("Camera_Pos_Z", cameraController.transform.position.z)
+        );
+        cameraController.transform.position = cameraPosition;
+
+        Vector3 cameraRotation = new Vector3(
+            PlayerPrefs.GetFloat("Camera_Rot_X", cameraController.transform.rotation.eulerAngles.x),
+            PlayerPrefs.GetFloat("Camera_Rot_Y", cameraController.transform.rotation.eulerAngles.y),
+            PlayerPrefs.GetFloat("Camera_Rot_Z", cameraController.transform.rotation.eulerAngles.z)
+        );
+        cameraController.transform.rotation = Quaternion.Euler(cameraRotation);
+
+        // Load Camera FOV
+        cameraController.mainCamera.fieldOfView = PlayerPrefs.GetFloat("Camera_FOV", cameraController.mainCamera.fieldOfView);
+
+        // Load Node Data - Reset all nodes to locked by default
+        foreach (var node in nodes)
+        {
+            bool isUnlocked = PlayerPrefs.GetInt($"Node_{node.name}_Unlocked", 0) == 1;
+            node.nodeUnlocked = isUnlocked; // Set to true if saved, else false
+            node.isClosed = PlayerPrefs.GetInt($"Node_{node.name}_Closed", 0) == 1;
+            node.forkUnlocked = PlayerPrefs.GetInt($"Node_{node.name}_ForkUnlocked", 0) == 1;
+            node.UpdateLightState(); // Update visuals
+        }
+        bool firstNodeUnlocked = PlayerPrefs.GetInt($"Node_{nodes[0].name}_Unlocked", -1) == 1;
+        if (firstNodeUnlocked == false)
+        {
+            // Unlock the first node by default if no saved data is found
+            nodes[0].nodeUnlocked = true;
+        }
+        else
+        {
+
+            foreach (var node in nodes)
+            {
+                node.nodeUnlocked = PlayerPrefs.GetInt($"Node_{node.name}_Unlocked", 0) == 1;
+                node.isClosed = PlayerPrefs.GetInt($"Node_{node.name}_Closed", 0) == 1;
+                node.forkUnlocked = PlayerPrefs.GetInt($"Node_{node.name}_ForkUnlocked", 0) == 1;
+                node.UpdateLightState(); // Update visuals
+            }
+        }
+    }
+
 }
