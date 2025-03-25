11203b77200981afb4ff5b3823b3fb3f1b6eebc9
Phoenixxi <rowan809xi@gmail.com>
2025-03-24 22:35:09 -0600

    Characters appear in Dialogue

--- Assets/J_Scipts/Dialogue.cs
+++ Assets/J_Scipts/Dialogue.cs
@@ -10,13 +10,23 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
-    private int index;
+    private int index = 0;
     private bool isDialogueActive = false;
+    public List<GameObject> imageList = new List<GameObject>();
+
+    private int dialogueNum = 2;
+
+
 
     void Start()
     {
         textComponent.text = string.Empty;
         gameObject.SetActive(false);  // Make sure the dialogue UI is hidden initially
+        int arraySize = lines.Length;
+        if(arraySize == 10)
+            dialogueNum = 2;
+        else if(arraySize == 4)
+            dialogueNum = 4;
     }
 
     // Update is called once per frame
@@ -57,6 +67,10 @@ public class Dialogue : MonoBehaviour
         if (index < lines.Length - 1)
         {
             index++;
+            if(dialogueNum == 2)
+                DisplayCharacterNodeTwo(index);
+            else if(dialogueNum == 4)
+                DisplayCharacterNodeFour(index);
             textComponent.text = string.Empty;
             StartCoroutine(TypeLine());
         }
@@ -65,6 +79,74 @@ public class Dialogue : MonoBehaviour
             isDialogueActive = false;  // Set flag to false when dialogue finishes
             gameObject.SetActive(false);  // Hide the dialogue UI after last line
 
+        }
+    }
+
+    public void DisplayCharacterNodeTwo(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                imageList[0].SetActive(true);
+                break;
+            case 1:
+                imageList[0].SetActive(true);
+                break;
+            case 2:
+                imageList[0].SetActive(false);
+                imageList[1].SetActive(true);
+                break;
+            case 3:
+                imageList[1].SetActive(false);
+                imageList[2].SetActive(true);
+                break;
+            case 4:
+                imageList[2].SetActive(false);
+                imageList[3].SetActive(true);
+                break;
+            case 5:
+                imageList[3].SetActive(false);
+                imageList[2].SetActive(true);
+                break;
+            case 6:
+                imageList[2].SetActive(false);
+                imageList[0].SetActive(true);
+                break;
+            case 7:
+                imageList[0].SetActive(false);
+                imageList[3].SetActive(true);   
+                break;
+            case 8:
+                imageList[0].SetActive(false);
+                imageList[3].SetActive(true);
+                break;
+            case 9:
+                imageList[3].SetActive(false);
+                imageList[2].SetActive(true);
+                break;
+            
+        }
+    }
+
+    public void DisplayCharacterNodeFour(int index)
+    {
+         switch(index)
+        {
+            case 0:
+                imageList[3].SetActive(true);
+                break;
+            case 1:
+                imageList[3].SetActive(false);
+                imageList[2].SetActive(true);
+                break;
+            case 2:
+                imageList[2].SetActive(false);
+                imageList[1].SetActive(true);
+                break;
+            case 3:
+                imageList[1].SetActive(false);
+                imageList[0].SetActive(true);
+                break;
         }
     }
 
