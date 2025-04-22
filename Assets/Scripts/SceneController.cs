86d88aa9abe360d9fede49ad3f6059894a480334
Phoenixxi <rowan809xi@gmail.com>
2025-04-21 19:18:07 -0600

    return to book working. Cannot click on next node

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -8,6 +8,13 @@ public class SceneController : MonoBehaviour
 {
      private float changeTime = 4.5f;
 
+    private int worldID;
+
+    void Start()
+    {
+        NodeController activeNode = MapManager.Instance.nodes[0].sendCurrentNode();
+        worldID = activeNode.thisWorld;
+    }
    public void BookOneButton()
    {
        SceneManager.LoadScene("Jared");
@@ -21,6 +28,16 @@ public class SceneController : MonoBehaviour
    public void BookThreeButton()
    {
        SceneManager.LoadScene("Map3");
+   }
+
+   public void BackToBook()
+   {
+        if(worldID == 0)
+            BookOneButton();
+        else if(worldID == 1)
+            BookTwoButton();
+        else if(worldID == 2)
+            BookThreeButton();
    }
 
 }
