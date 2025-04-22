69a267d0decea50ad90166a4ed23c99ed8dac8b5
Ghskljd <ryannip86@gmail.com>
2025-04-21 22:55:27 -0700

    Added Dialogue to world 1

--- Assets/J_Scipts/Dialogue.cs
+++ Assets/J_Scipts/Dialogue.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI characterName;
     public string[] lines;
     public float textSpeed;
     private int index = 0;
@@ -46,6 +47,7 @@ public class Dialogue : MonoBehaviour
     {
         Dialgoue = Instantiate(DialoguePrefab, transform);
         textComponent = Dialgoue.transform.Find("DialogueText").GetComponent<TextMeshProUGUI>();
+        characterName = Dialgoue.transform.Find("CharacterNameText").GetComponent<TextMeshProUGUI>();
         index = 0;
         textComponent.text = string.Empty;  // Clear any previous text
         textComponent.text = lines[index];
@@ -76,6 +78,7 @@ public class Dialogue : MonoBehaviour
         else
         {
             isDialogueActive = false;  // Set flag to false when dialogue finishes
+            Destroy(Dialgoue);
             gameObject.SetActive(false);  // Hide the dialogue UI after last line
 
         }
@@ -101,33 +104,43 @@ public class Dialogue : MonoBehaviour
         {
             case 1:
                 Dialgoue.transform.Find("Characters").Find("Mewa").gameObject.SetActive(true);
+                characterName.text = "Mewa";
                 break;
             case 2:
                 Dialgoue.transform.Find("Characters").Find("Eou").gameObject.SetActive(true);
+                characterName.text = "Eou";
                 break;
             case 3:
                 Dialgoue.transform.Find("Characters").Find("BellaBora").gameObject.SetActive(true);
+                characterName.text = "Bella Bora";
                 break;
             case 4:
                 Dialgoue.transform.Find("Characters").Find("King").gameObject.SetActive(true);
+                characterName.text = "King Fire Blast";
                 break;
             case 5:
                 Dialgoue.transform.Find("Characters").Find("Sviur").gameObject.SetActive(true);
+                characterName.text = "Sviur";
                 break;
             case 6:
                 Dialgoue.transform.Find("Characters").Find("Lune").gameObject.SetActive(true);
+                characterName.text = "Lune";
                 break;
             case 7:
                 Dialgoue.transform.Find("Characters").Find("Elio").gameObject.SetActive(true);
+                characterName.text = "Elio";
                 break;
             case 9:
                 Dialgoue.transform.Find("Characters").Find("Xue").gameObject.SetActive(true);
+                characterName.text = "Xue";
                 break;
             case 10:
                 Dialgoue.transform.Find("Characters").Find("BB").gameObject.SetActive(true);
+                characterName.text = "Boris & Berta";
                 break;
             case 11:
                 Dialgoue.transform.Find("Characters").Find("BigBoss").gameObject.SetActive(true);
+                characterName.text = "???";
                 break;
         }
     }
